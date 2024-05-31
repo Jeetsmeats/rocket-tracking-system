8b94040c066910c2068124f825a2457f5dd8681f
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-05-31 14:40:11 +1000

    Read HackRF stream
    - Reading 10 sample stream from 1 board
    - Difficulty reading from both HackRF A & B :expressionless:

--- HackRF.py
+++ HackRF.py
@@ -1,18 +1,40 @@
+# Imports
+import SoapySDR
+
 class HackRF(object):
     """_summary_
     
     Class wrapper for the HackRF Boards
     """
     
-    def __init__(self, board, name, serial_num):
+    def __init__(self, name, serial_tag):
         """_summary_
 
         Instantiate a HackRF Board
         Args:
             board (_type_): _description_
             name (_type_): _description_
-            serial_num (_type_): _description_            
+            serial_tag (string): _description_            
         """
-        self.board = board
+        arg = dict(driver="hackrf", serial=serial_tag)
+        self.board = SoapySDR.Device(arg)
         self.name = name
-        self.serial_num = serial_num
+        self.serial_tag = serial_tag
+        
+    def get_board(self):
+        """_summary_
+        
+        Get the HackRF Board instance
+        Returns:
+            _type_: HackRF Board
+        """
+        return self.board
+
+    def get_name(self):
+        """_summary_
+
+        Get the board name
+        Returns:
+            str: Board Name
+        """
+        return self.name
--- main.py
+++ main.py
@@ -6,62 +6,77 @@ import matplotlib.pyplot as plt
 import SoapySDR
 from SoapySDR import *          #SOAPY_SDR_ constants
 
+# Import Modules
+from HackRF import HackRF
+
 def main():
-    pass
-
-if __name__ == "main":
-    pass
-
-# Enumerate over visible devices
-# results = SoapySDR.Device.enumerate()
-
-# # Print the SoapySDR Results
-# for result in results: 
-#     print(result)
     
-# Create a Driver Instance
-arg1 = dict(driver="hackrf", serial="0000000000000000675c62dc335c76cf")
-arg2 = dict(driver="hackrf", serial="0000000000000000675c62dc302745cf")
-# arg3 = dict(driver="hackrf", serial="0000000000000000675c62dc304807cf")
-# arg4 = dict(driver="hackrf", serial="0000000000000000")
+    # Enumerate over visible devices
+    results = SoapySDR.Device.enumerate()
 
-# Create an instance of each hackrf
-hackrf_A = SoapySDR.Device(arg1)
-hackrf_B = SoapySDR.Device(arg2)
-# hackrf_C = SoapySDR.Device(arg3)
-# hackrf_D = SoapySDR.Device(arg4)
-
-sdr = {
-    'HackRF A': hackrf_A,
-    'HackRF B': hackrf_B,
-    # 'HackRF C': hackrf_C,
-    # 'HackRF D': hackrf_D,
-}
-
-# HackRF Streams
-streams = []
-
-# Retrieve device information for each detected HackRF
-for device in sdr:
+    # Print the SoapySDR Results
+    for result in results: 
+        print(result)
     
-    # Retrieve selected HackRF
-    hackrf = sdr[device]
-    
-    print(hackrf.getDriverKey())
-    # Print hackrf board settings
-    fq_range = hackrf.getFrequencyRange(SOAPY_SDR_RX, 0) 
-    ant_det = hackrf.listAntennas(SOAPY_SDR_RX, 0)
-    
-    # Print settings
-    print(f'\nHACKRF DEVICE: {device}\n',
-          '------------------------------')
-    print(f'Frequency Range: {fq_range}')
-    print(f'Antenna Details: {ant_det}')
-    
-    # Apply settings
-    hackrf.setSampleRate(SOAPY_SDR_RX, 0, 20e6)
-    hackrf.setFrequency(SOAPY_SDR_RX, 0, 9.15e6)
+    if input("Correct HackRF's? (Y/N) ").lower() == "y" or input("Correct HackRF's? (Y/N) ").lower() == "yes":
+        
+        # Create an instance of each hackrf
+        hackrf_1 = HackRF("HackRF A", "0000000000000000675c62dc335c76cf")
+        hackrf_2 = HackRF("HackRF B", "0000000000000000675c62dc302745cf")
+        hackrf_3 = HackRF("HackRF C", "0000000000000000675c62dc304807cf")
+        # hackrf_4 = HackRF("HackRF D", "0000000000000000################")
 
-    # Setup hackrf stream
-    rx_stream = hackrf.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF64)
-    streams.append(rx_stream)
+        sdr = {
+            hackrf_1.get_name(): hackrf_1,
+            hackrf_2.get_name(): hackrf_2,
+            hackrf_3.get_name(): hackrf_3,
+            # hackrf_4.get_name(): hackrf_4,
+        }
+
+        # HackRF Streams
+        streams = []
+
+        # Retrieve device information for each detected HackRF
+        for device in sdr:
+            
+            # Retrieve selected HackRF
+            hackrf = sdr[device].get_board()
+            
+            # Get current board settings
+            fq_range = hackrf.getFrequencyRange(SOAPY_SDR_RX, 0) 
+            ant_det = hackrf.listAntennas(SOAPY_SDR_RX, 0)
+            
+            # Print settings
+            print(f'\nHACKRF DEVICE: {device}\n',
+                '------------------------------')
+            print(f'Frequency Range: {fq_range}')
+            print(f'Antenna Details: {ant_det}')
+            
+            # Apply settings
+            hackrf.setSampleRate(SOAPY_SDR_RX, 0, 20e6)
+            hackrf.setFrequency(SOAPY_SDR_RX, 0, 9.15e6)
+
+            # Setup a stream (complex floats)
+            stream = hackrf.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF64)
+            streams.append(stream)
+
+            # Start streaming 
+            hackrf.activateStream(stream)
+            
+            # Create a re-usable buffer for rx samples (Pre-allocate Memory)
+            buff = np.array([0]*1024, np.complex64)
+
+            print(f'Samples from {device}')
+            # Retrieve 10 samples
+            for i in range(10):
+                
+                samples = hackrf.readStream(stream, [buff], len(buff))
+                
+                print(samples) # num samples or error code
+                
+            # Close the stream
+            hackrf.deactivateStream(stream) #stop streaming
+            hackrf.closeStream(stream)
+            
+if __name__ == "__main__":
+    main()
