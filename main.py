656f544550d916f4321c53427f35c16e4dbfeaaf
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-06-11 15:39:04 +1000

    Fixed main.py to account for module repairs
    - Visualiser
    - Processor
    - FFT

--- main.py
+++ main.py
@@ -9,21 +9,12 @@ from SoapySDR import *          #SOAPY_SDR_ constants
 from HackRF import HackRF
 from Processor import Processor
 from Visualiser import Visualiser                    
+from FFT import FFT
 
 def main():
     """_summary_
     Main function for running RTS
     """
-    
-    ## Constants   
-    NUM_SAMPLES = 5000              # Number of samples
-    PLOT_SAMPLES = 5                # Number of samples to plot
-    N = 1024                        # Number of IQ data points
-    NUM_DEVICES = len(sdr)          # Number of connected devices
-    CHANNEL = 0                     # Antenna channel
-    CENTRE_FREQ = 900e6             # Center Frequency
-    BANDWIDTH = 10e6                # Bandwidth
-    SAMPLE_RATE = 10e6              # Sample Rate
     
     ## HackRF Boards
     # Default HackRF Instances
@@ -31,15 +22,25 @@ def main():
     hackrf_2 = HackRF("HackRF B", "0000000000000000675c62dc302745cf")
     hackrf_3 = HackRF("HackRF C", "0000000000000000675c62dc304807cf")
     # hackrf_4 = HackRF("HackRF D", "0000000000000000################")
-
+    
     # SDR Dictionary
-    sdr = {
+    sdr = {    ## Main Logic
         hackrf_1.get_name(): hackrf_1,
         hackrf_2.get_name(): hackrf_2,
         # hackrf_3.get_name(): hackrf_3,
         # hackrf_4.get_name(): hackrf_4,
     }
     
+    ## Constants   
+    NUM_SAMPLES = 5000              # Number of samples
+    PLOT_SAMPLES = 5                # Number of samples to plot
+    N = 1024                        # Number of IQ data points
+    NUM_DEVICES = len(sdr)          # Number of connected devices
+    CHANNEL = 0                     # Antenna channel
+    CENTRE_FREQ = 915e6           # Center Frequency
+    BANDWIDTH = 10e6                # Bandwidth
+    SAMPLE_RATE = 10e6              # Sample Rate
+
     # Board Names
     BOARD_NAMES = [board_name for board_name in sdr]    
     
@@ -65,20 +66,24 @@ def main():
     ## Data objects
     
     ## Main Logic
+    
+    # COMPLETE LOOP FOR REAL TIME TRACKING
     # while True:
     #     pass
     
-    
+    ## Processing 
     processor.activate_boards()
-    processor.collect_samples()
-    processor.deactivate_boards()     
     
-    visuals.create_figure("IQ Constellation")
-    visuals.create_figure("FFT Plot")
+    fft = processor.collect_samples()
+    data = processor.get_data()
+    freq = processor.get_frequency()
     
-    visuals.plot_argand_diagram("IQ Constellation", )
-    visuals.plot_fft("FFT Plot", )
-        
+    fft_sample = fft.get_fft_sample()
+    
+    processor.deactivate_boards()   
+    
+    visuals.plot_IQ_constellation("IQ (HackRF A Clock Off) - $TEST 2$",data)
+    visuals.plot_fft("FFT (HackRF A Clock Off) - $TEST 2$",fft_sample, freq)
     
 if __name__ == "__main__":
     main()
