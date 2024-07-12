7b721fedaf337b0a0ed3caf9ac7c3efbcc7058e2
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-07-12 16:59:34 +1000

    Linux pipe starter

--- shell_files/trigger_A.sh
+++ shell_files/trigger_A.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hackrf_transfer -d 0000000000000000675c62dc335c76cf -a 0 -f 915000000 -s 20000000 -r 76cf.raw
+hackrf_transfer -d 0000000000000000675c62dc335c76cf -a 0 -f 915000000 -s 20000000 -r hackrfA_pipe
--- test_raw_extract.py
+++ test_raw_extract.py
@@ -1,64 +1,15 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from multiprocessing import Process
-import time
-import os
+from time import time
+import subprocess
 
-hackrf_A_file = "./shell_files/76cf.raw"
-hackrf_C_file = "./shell_files/07cf.raw"
+start = time()
 
-# os.popen(start_command)
-# os.popen(trigger_C_command)
+trigger_A = ['hackrf_transfer', '-d', '0000000000000000675c62dc335c76cf', '-a', '0', '-f', '915000000', '-s', '20000000', '-r', './pipes/hackrfA_pipe']
+hackrf_A_stream = subprocess.Popen(trigger_A, stdout=subprocess.PIPE)
 
-# time.sleep(2)
-
-# os.popen(trigger_A_command)
-
-# time.sleep(10)
-
-# opening the file in read mode
-# hackrf_A = open(hackrf_A_file, "rb+")
-# hackrf_C = open(hackrf_C_file, "rb+")
-
-start = time.time()
-
-n_bytes = 1600000
-while True:
-    
-    # hackrf_A.flush()
-    # hackrf_C.flush()
-    
-    hackrf_A_data = np.fromfile(hackrf_A_file, count=n_bytes, dtype="int8")
-    hackrf_C_data = np.fromfile(hackrf_C_file, count=n_bytes, dtype="int8")
-    
-    print(hackrf_A_data)
-    print(hackrf_C_data)
-    
-    # hackrf_A.truncate(0)
-    # hackrf_C.truncate(0)
-    
-    # hackrf_A.write(('\0' * len(hackrf_A_data)).encode())
-    # hackrf_C.write(('\0' * len(hackrf_C_data)).encode())
-    
-    # print(len(hackrf_A.read()))
-    # print(len(hackrf_C.read()))
-    
-    # print(time.time() - start)
-    # hackrf_A_data = np.array(hackrf_A_buffer).astype(np.int8)
-    # hackrf_C_data = np.array(hackrf_C_buffer).astype(np.int8)
-    
-    time.sleep(0.5)
-    
-# plt.subplot(1, 2, 1)
-
-# plt.plot(hackrf_A_data)
-# plt.title("HackRF A Data")
-# plt.xlim((0, 100))
-
-# plt.subplot(1, 2, 2)
-
-# plt.plot(hackrf_C_data, color="red")
-# plt.title("HackRF C Data")
-# plt.xlim((0, 100))
-
-# plt.show()
+for iq_samples in iter(lambda: bytearray(hackrf_A_stream.stdout.read(8 * 1024)), b''):
+        # convert the samples chunk for use by numpy, if you wish
+        data = np.array(iq_samples)
+        print(data)
