517e86ec6be0989ec13bf36e47f4bc69f3fb181b
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-08-01 17:47:18 +1000

    Adding trigger for hackrf D

--- shell_files/trigger_D.sh
+++ shell_files/trigger_D.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hackrf_transfer -d 0000000000000000675c62dc30819dcf -a 0 -f 915000000 -s 10000000 -r pipes/hackrfD.pipe
+hackrf_transfer -H -d 0000000000000000675c62dc30819dcf -a 0 -f 915000000 -s 10000000 -r pipes/hackrfD.pipe
 
