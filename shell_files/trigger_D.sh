720c3da1dcabdb866e29b35dcdf4db61ec5d20db
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-08-02 16:13:32 +1000

    First integrated test
    - Partially Successful
    - Raspberry Pi to Hackrf data acquisition limit to 2 HackRFs

--- shell_files/trigger_D.sh
+++ shell_files/trigger_D.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 hackrf_transfer -H -d 0000000000000000675c62dc30819dcf -a 0 -f 915000000 -s 10000000 -r pipes/hackrfD.pipe
-
--- test_files/test_data_pipeline.py
+++ test_files/test_data_pipeline.py
@@ -6,36 +6,43 @@ import paho.mqtt.client as paho
 from paho import mqtt
 import random
 import time
+import subprocess
 
-def gen_signal(id, topic, board):
-    broker = "192.168.1.142"
+def process_signal(id, topic, board, pipe_path, address):
+    broker = address
     port = 1883
 
     client = paho.Client(client_id=id, protocol=paho.MQTTv5)
     client.on_connect = on_connect
     client.connect(broker, port)
 
-    f = 100
-    w = 2 * np.pi * f
-    t = np.linspace(0, 1, 200)
-    x = t * np.pi / 180
+    f = 915000000
 
-    while True:
-        random_phase = random.uniform(0, np.pi / 2)
+    with open(pipe_path, 'rb') as pipe:
+        
+        data = np.array([])
+        
+        while True:
+            
+            raw_data = bytearray(pipe.read(8*1024))
+            data = np.append(data, np.array(raw_data).astype(np.int8).astype(np.float64).view(np.complex128))
 
-        sig = np.sin(w * x + random_phase)
+            if len(data) > 100000:
+                
+                I = data[0::2]
+                Q = data[1::2]
+                
+                
+                fft_signal = fft(data, 1024)
+                
+                real = np.real(fft_signal[len(fft_signal) // 2])
+                imag = np.imag(fft_signal[len(fft_signal) // 2])
 
-        random_noise = np.random.normal(0, 1, len(t))
-
-        noisy_signal = sig + random_noise
-
-        fft_signal = fft(noisy_signal, 4096)
-        fft_shifted = fftshift(fft_signal)
-        phase = random_phase
-
-        client.publish(topic, f'Phase value {random_phase} from {board}')
-
-        time.sleep(1)
+                client.publish(topic, f"Board: {board} Real: {real}, Imag: {imag}")
+                
+                data = np.array([])
+                time.sleep(0.05)
+                
 
 def on_connect(rc):
 
@@ -46,17 +53,37 @@ def on_connect(rc):
 
 
 def main():
-    topic = "test/topic1"
+    topic = "test/topic"
+    
+    mqtt_id_1 = "4083e8a8d0dc"
+    mqtt_id_2 = "fe91eb8bc508"
+    mqtt_id_3 = "f265020c6a9c"
+    mqtt_id_4 = "ad13a4cc4122"
+    
+    pipe_A_path = "./shell_files/pipes/hackrfA.pipe"
+    pipe_B_path = "./shell_files/pipes/hackrfB.pipe"
+    pipe_C_path = "./shell_files/pipes/hackrfC.pipe"
+    pipe_D_path = "./shell_files/pipes/hackrfD.pipe"
+    
+    mqtt_address = "10.12.23.21"
 
-    process1 = Process(target=gen_signal, args=("4083e8a8d0dc", topic, "board A"))
-    process2 = Process(target=gen_signal, args=("fe91eb8bc508", topic, "board B"))
-    process3 = Process(target=gen_signal, args=("f265020c6a9c", topic, "board C"))
+    # process1 = Process(target=process_signal, args=(mqtt_id_1, topic, "board D", pipe_D_path , mqtt_address))
+    # process2 = Process(target=process_signal, args=(mqtt_id_2, topic, "board C", pipe_C_path , mqtt_address))
+    process3 = Process(target=process_signal, args=(mqtt_id_3, topic, "board B", pipe_B_path , mqtt_address))
 
-    process1.start()
-    process2.start()
+    # print("Starting HackRF D")
+    # process1.start()
+    # time.sleep(1)
+    
+    # print("Starting HackRF C")
+    # process2.start()
+    # time.sleep(1)
+    
+    print("Starting HackRF B")
     process3.start()
-
-    gen_signal("ad13a4cc4122", topic, "board D")
+    
+    print("Starting HackRF A")
+    process_signal(mqtt_id_4, topic, "board A", pipe_A_path , mqtt_address)
 
 if __name__ == "__main__":
     main()
