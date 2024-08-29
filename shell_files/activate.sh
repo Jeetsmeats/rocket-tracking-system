1a2ca1de0576a832cd668a0ae2af938d39da0115
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-08-29 19:05:53 +1000

    Remote script trigger from master raspberry pi successful
    - Unsuccessful at triggering ptp remotely.  (Sudo issue)

--- shell_files/activate.sh
+++ shell_files/activate.sh
@@ -2,6 +2,7 @@
 
 RPI1="10.13.124.175"
 RPI2="10.12.42.202"
+echo "Set up RPi IP Addresses"
 
 run_command() {
 	
@@ -11,36 +12,37 @@ run_command() {
 	ssh Jeetsmeats@$RPI "$COMMAND"
 }
 
-# Subscribe to test/topic in background
-mosquitto_sub -d -t "test/topic" 
-
 # Start HackRF B Internal Clock
-run_command $RPI1 "bash /Documents/rocket-tracking-system/shell_files/start.sh"
+run_command $RPI1 "bash ~/Documents/rocket-tracking-system/shell_files/start.sh"
+echo "Activated HackRF B master clock"
 
-# Set up Raspberry Pi 1 as PTP master
-run_command $RPI1 "bash sudo ptp4l -i eth0 --masterOnly 1 -m --tx_timestamp_timeout 200"
-sleep(1)
+# Set up Raspberry Pi 1 as PTP master, run as P2P instead of E2E
+run_command $RPI1 "bash sudo ptp4l -i eth0 -m 1 -P"
+sleep 1s
+echo "Set up PTP master Raspberry Pi 1"
 
 # Set up Raspberry Pi 2 as PTP slave
-run_command $RPI2 "bash sudo ptp4l -i eth0 --slaveOnly 1 -m --tx_timestamp_timeout 200"
-
-sleep(30)
+run_command $RPI2 "bash sudo ptp4l -i eth0 -m 0 -P"
+echo "Set up PTP slave Raspberry Pi 2, awaiting synchronisation completion..."
+sleep 15s
 
 # Run HackRF D trigger
-run_command $RPI2 "bash /Documents/rocket-tracking-system/shell_files/trigger_D.sh"
-sleep(1)
+run_command $RPI2 "bash ~/Documents/rocket-tracking-system/shell_files/trigger_D.sh"
+sleep 1s
+echo "Triggered HackRF D!"
 
 # Run HackRF C trigger
-run_command $RPI2 "bash /Documents/rocket-tracking-system/shell_files/trigger_C.sh"
-sleep(1)
+run_command $RPI2 "bash ~/Documents/rocket-tracking-system/shell_files/trigger_C.sh"
+sleep 1s
+echo "Trigger HackRF C!"
 
 # Run Hack B and A trigger
-(run_command $RPI1 "bash /Documents/rocket-tracking-system/shell_files/trigger_A.sh") &
-(run_command $RPI1 "bash /Documents/rocket-tracking-system/shell_files/trigger_B.sh") &
-sleep(0.5)
+(run_command $RPI1 "bash ~/Documents/rocket-tracking-system/shell_files/trigger_A.sh") &
+(run_command $RPI1 "bash ~/Documents/rocket-tracking-system/shell_files/trigger_B.sh") &
+sleep 1s
+echo "Trigger HackRF B and C!"
 
 # Run Data Acquisition Files
-(run_command $RPI1 "python3 /Documents/rocket-tracking-system/test_files/test_rpi.py") &
-(run_command $RPI2 "python3 /Documents/rocket-tracking-system/test_files/test_rpi2.py) &
-
-
+(run_command $RPI1 "python3 ~/Documents/rocket-tracking-system/test_files/test_rpi.py") &
+(run_command $RPI2 "python3 ~/Documents/rocket-tracking-system/test_files/test_rpi2.py) &
+echo "Executed data collection files and collecting data!"
